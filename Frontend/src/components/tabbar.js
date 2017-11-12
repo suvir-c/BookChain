0883a298bdbe3253812e4c29b2c69f826c76de88
John Zeiders <zeiders@usc.edu>
2017-11-12 15:56:45 -0800

    updated user profile

--- Frontend/src/components/tabbar.js
+++ Frontend/src/components/tabbar.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { TabBar } from "react-native-tab-view";
+import { StyleSheet } from "react-native";
 
 export default class MainTabBar extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return <TabBar {...this.props} />;
+    return (
+      <TabBar
+        {...this.props}
+        style={styles.TabBar}
+        indicatorStyle={styles.Indicator}
+      />
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  TabBar: {
+    backgroundColor: "#FF6659"
+  },
+  Indicator: {
+    backgroundColor: "#fff"
+  }
+});
